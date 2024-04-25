b1e3e90a4e0c77f4ac0aea53b8a888b0e0fe8efd
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-04-24 22:04:47 -0300

    Add SQL migration script to insert test data into rac_racao table

---
+++ src/main/resources/db/migration/V3__add_rows_to_tests.sql
@@ -0,0 +1,15 @@
+-- adicionando inserts de teste no formato:
+-- insert into rac_racao(rac_marca, rac_data_hora_ultima_compra, rac_quantidade_estoque, rac_nota)
+-- values ('Purina', now(), 10.5, 7),
+--        ('Pedigree', '2024-03-15T23:59:59', null, 5);
+
+insert into rac_racao(rac_marca, rac_data_hora_ultima_compra, rac_quantidade_estoque, rac_nota)
+values
+    ('Royal Canin', '2024-03-15T23:59:59', 20.5, 8),
+    ('Hills', '2024-03-15T23:59:59', 15.5, 6),
+    ('Golden', '2024-03-15T23:59:59', 5.5, 9),
+    ('Premier', '2024-03-15T23:59:59', 3.5, 4),
+    ('N&D', '2024-03-15T23:59:59', 7.5, 10),
+    ('Biofresh', '2024-03-15T23:59:59', 2.5, 3),
+    ('Farmina', '2024-03-15T23:59:59', 1.5, 2),
+    ('Equilíbrio', '2024-03-15T23:59:59', 4.5, 1);
